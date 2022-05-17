f479904539abece4937b590bcacc1810310f07cc
mguerra <msg4@poli.br>
2022-05-17 20:46:15 -0300

    Validações (falta finalizar)

--- SITE MMM/js/scripts.js
+++ SITE MMM/js/scripts.js
@@ -1,12 +1,35 @@
-class Validator(){
+class Validator{
 
     constructor(){
-        this.validations[
+        this.validations = [
+
+            'data-min-length'
 
         ]
     }
 
     //iniciar a validação de todos os campos
+    validate(form){
+
+        //pegar os inputs
+        let inputs = document.getElementsByName('input');
+
+        //transforma HTMLcollection -> array
+        let inputsArray = [...inputs];
+
+        //loop nos inputs e validação no que for encontrado
+        inputsArray.forEach(function(input) {
+            //loop em todas as validações existentes
+            for(let i = 0; this.validations.length > i; i++){
+
+                if(input.getAttribute(this.validations[i]) != null){
+
+                }
+
+            }
+        }, this);
+
+    }
 
 }
 
