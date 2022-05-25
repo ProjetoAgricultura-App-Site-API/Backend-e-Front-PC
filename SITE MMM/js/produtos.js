2e186002afffbe576bd1eea8f8fa5d15f2e0d354
mguerra <msg4@poli.br>
2022-05-24 23:38:49 -0300

    Limpa form após o clique

--- SITE MMM/js/produtos.js
+++ SITE MMM/js/produtos.js
@@ -1,14 +1,19 @@
 function puxaproduto(){
-    let produto = document.getElementById('produto').value;
-    let quant = document.getElementById('quant').value;
-    let data = document.getElementById('data').value;
+    let produto = document.getElementById('produto');
+    let quant = document.getElementById('quant');
+    let data = document.getElementById('data');
 
     let req = {
-        Produto: produto,
-        Quantidade: quant,
-        Data: data
+        Produto: produto.value,
+        Quantidade: quant.value,
+        Data: data.value
     }
 
     console.log(req);
+
+    document.getElementById('produto').value = '';
+    document.getElementById('quant').value = '';
+    document.getElementById('data').value = '';
+    produto.focus()
 
 }
