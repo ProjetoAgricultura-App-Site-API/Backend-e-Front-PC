655a48cfdc1b87c370321ca64eebfabbdb2edbe2
mguerra <msg4@poli.br>
2022-05-27 19:40:00 -0300

    mudança na api de cadastro

--- SITE MMM/js/scripts.js
+++ SITE MMM/js/scripts.js
@@ -3,6 +3,7 @@
 //GUARDA O CADASTRO E REDIRECIONA PARA TELA DE LOGIN
 function guardaCadastro() {
     
+    const url = "http://localhost:5500/api";
     var senha = document.getElementById("password");
     let msgerror = document.getElementById("msg-error");
     var nomeValor = document.getElementById("name").value;
@@ -21,6 +22,16 @@ function guardaCadastro() {
         }
 
         //ADICIONA OS VALORES AO LOCAL STORAGE
+        function newuser(){
+            axios.post(url, formValue)
+            .then(response => {
+                console.log("USER CADASTRADO");
+            })
+            .catch(error => error)
+        }
+
+        newuser();
+
         listauser.push(formValue);
         localStorage.setItem('listauser', JSON.stringify(listauser));
 
