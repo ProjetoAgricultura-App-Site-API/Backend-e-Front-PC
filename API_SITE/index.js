c1f74bda5eeb6a3ff8c344c6570841bd92f7116d
mguerra <msg4@poli.br>
2022-06-03 17:46:58 -0300

    Alterações no back end do APP

---
+++ API_SITE/index.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.listen(5500, () => console.log('Rodando na porta http://localhost:5500/api'))
+
+app.use(cors())
+
+app.use(express.json())
+
+//LÊ OS DADOS DO .JSON PARA NÃO PERDER DADOS NOVOS
+fs.readFile("users.json", function(err, data){ //PUXA DADOS DOS USUÁRIOS DO SITE
+  if(err){
+    throw err;
+  }
+
+  users = JSON.parse(data);
+})
+
+app.route('/api').get((req, res) => res.json({ //SITE
+  users
+}))
+
+app.route('/api/:id').get((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  res.json(user)
+})
+
+app.route('/api').post((req, res) => { //CADASTRA NOVOS USUÁRIOS DO SITE
+  const lastId = users[users.length - 1].id
+  users.push({
+    id: lastId + 1,
+    Nome: req.body.Nome,
+    Sobrenome: req.body.Sobrenome,
+    Email: req.body.Email,
+    Senha: req.body.Senha
+  })
+  res.json('Saved user')
+  fs.writeFile("users.json",JSON.stringify(users), err => { //ESCREVE DADOS RECEBIDOS PELA API NO .JSON
+    if (err) {throw err;} 
+  })
+})
+
+
+app.route('/api/:id').put((req, res) => {
+  const userId = req.params.id
+
+  const user = users.find(user => Number(user.id) === Number(userId))
+
+  if (!user) {
+    return res.json('User nor found!')
+  }
+
+  const updatedUser = {
+    ...user,
+    Nome: req.body.Nome,
+    Sobrenome: req.body.Sobrenome,
+    Email: req.body.Email,
+    Senha: req.body.Senha
+  }
+
+  users = users.map(user => {
+    if (Number(user.id) === Number(userId)) {
+      user = updatedUser
+    }
+    return user
+  })
+
+  res.json("Updated user")
+})
+
+app.route('/api/:id').delete((req, res) => {
+  const userId = req.params.id
+
+  users = users.filter(user => Number(user.id) !== Number(userId))
+
+  res.json('Deleted User')
+})
--- APP_API/index.js
+++ APP_API/index.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 
 const app = express()
 
-app.listen(5500, () => console.log('Rodando na porta http://localhost:5500/api'))
+app.listen(5700, () => console.log('Rodando na porta http://localhost:5700/api'))
 
 app.use(cors())
 
 app.use(express.json())
 
 //LÊ OS DADOS DO .JSON PARA NÃO PERDER DADOS NOVOS
-fs.readFile("users.json", function(err, data){ //PUXA DADOS DOS USUÁRIOS DO SITE
+fs.readFile("app.json", function(err, data){ //PUXA DADOS DOS USUÁRIOS DO SITE
   if(err){
     throw err;
   }
@@ -19,22 +19,8 @@ fs.readFile("users.json", function(err, data){ //PUXA DADOS DOS USUÁRIOS DO SIT
   users = JSON.parse(data);
 })
 
-fs.readFile("app_users.json", function(err, data){ //PUXA DADOS DOS USUÁRIOS DO APP
-  if(err){
-    throw err;
-  }
-
-  app_users = JSON.parse(data);
-})
-
-
-
 app.route('/api').get((req, res) => res.json({ //SITE
   users
-}))
-
-app.route('/api/APP').get((req, res) => res.json({ //APP
-  app_users
 }))
 
 app.route('/api/:id').get((req, res) => {
@@ -53,32 +39,19 @@ app.route('/api').post((req, res) => { //CADASTRA NOVOS USUÁRIOS DO SITE
   const lastId = users[users.length - 1].id
   users.push({
     id: lastId + 1,
+    Login: req.body.Login,
+    Senha: req.body.Senha,
     Nome: req.body.Nome,
     Sobrenome: req.body.Sobrenome,
     Email: req.body.Email,
-    Senha: req.body.Senha
+    Telefone: req.body.Telefone
   })
   res.json('Saved user')
-  fs.writeFile("users.json",JSON.stringify(users), err => { //ESCREVE DADOS RECEBIDOS PELA API NO .JSON
+  fs.writeFile("app.json",JSON.stringify(users), err => { //ESCREVE DADOS RECEBIDOS PELA API NO .JSON
     if (err) {throw err;} 
   })
 })
 
-app.route('/api/APP').post((req, res) => { //CADASTRA NOVOS USUÁRIOS DO APP
-  const lastId = users[users.length - 1].id
-  app_users.push({
-    id: lastId + 1,
-    Nome: req.body.Nome,
-    Sobrenome: req.body.Sobrenome,
-    Email: req.body.Email,
-    Numero: req.body.Numero,
-    Senha: req.body.Senha
-  })
-  res.json('Saved user')
-  fs.writeFile("app_users.json",JSON.stringify(users), err => { //ESCREVE DADOS RECEBIDOS PELA API NO .JSON
-    if (err) {throw err;} 
-  })
-})
 
 app.route('/api/:id').put((req, res) => {
   const userId = req.params.id
