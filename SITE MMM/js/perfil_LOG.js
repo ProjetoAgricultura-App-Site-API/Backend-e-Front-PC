1673faf814a30e24ea8384f88c9805a0e7eb206b
mguerra <msg4@poli.br>
2022-06-07 23:06:32 -0300

    Tela de perfil finalizada, falta put do axios

---
+++ SITE MMM/js/perfil_LOG.js
@@ -0,0 +1,10 @@
+
+let nome = document.getElementById('nome');
+let sobrenome = document.getElementById('sobrenome');
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+
+nome.innerHTML = userLogado.Nome;
+sobrenome.innerHTML = userLogado.Sobrenome
+email.innerHTML = userLogado.Email
+senha.innerHTML = userLogado.Senha
